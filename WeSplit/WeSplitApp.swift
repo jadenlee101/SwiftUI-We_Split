d5c6041bb6b55e3384f177d27b4164bbe6b4e35c
Jaden Lee <jaden.lee101@gmail.com>
2023-05-30 18:45:35 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jaden Lee on 2023-05-30.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
